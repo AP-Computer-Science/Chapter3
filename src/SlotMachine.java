1741e4dfc3541b022f39fa30ce2e4e583069eeec
Brian Barnes <brian.jx.b@gmail.com>
2018-10-23 14:19:51 -0500

    Added comps to slot machine

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 public class SlotMachine {
     public static void main(String[] args) {
-        final double PAYOUT_ODDS = 2;
-        final double BONUS_MULTIPLIER = PAYOUT_ODDS * 2;
-        final double MIN_LIMIT = 4;
-        final double MAX_LIMIT = 1000000;
+        final double PAYOUT_ODDS = 2.0;
+        final double BONUS_MULTIPLIER = PAYOUT_ODDS * 2.0;
+        final double MIN_LIMIT = 4.0;
+        final double MAX_LIMIT = 1000000.0;
+        final double COMP_FREE_DRINK = 10.0;
+        final double COMP_FREE_MOVIE = 32.0;
+        final double COMP_FREE_VACATION = 1.0;
         int wheel1;
         int wheel2;
         int wheel3;
+        int timesPlayed = 0;
+        int compTimesPlayed = 0;
         double money = 100.00;
         DecimalFormat df = (DecimalFormat)DecimalFormat.getCurrencyInstance();
         Scanner s = new Scanner(System.in);
@@ -30,24 +35,43 @@ public class SlotMachine {
                     System.out.println("Your bet is above the limit.");
                     continue;
                 }
-                money -= bet;
                 wheel1 = (int)(Math.random() * 10);
                 wheel2 = (int)(Math.random() * 10);
-                wheel3 = (int)(Math.random() * 10);
-                System.out.println("The wheels have turned: " + wheel1 + wheel2 + wheel3);
-                if((wheel1 == wheel2 && wheel1 == wheel3)
-                        && (wheel2 == wheel3 && wheel2 == wheel1)
-                        && (wheel3 == wheel1 && wheel3 == wheel2)) {
+                //wheel3 = (int)(Math.random() * 10);
+                System.out.println("The wheels have turned: " + wheel1 + wheel2);
+                if((wheel1 == wheel2)
+                        && (wheel2 == wheel1)) {
+                        //&& (wheel3 == wheel1 && wheel3 == wheel2)) {
                     if(wheel1 == 0) {
                         System.out.println("You got the JACKPOT!!!!!!!!!!!!!!!!!!!");
                         money += bet * BONUS_MULTIPLIER;
                     }
-                    System.out.println("You won!!!!!!");
-                    money += bet * PAYOUT_ODDS;
+                    else {
+                        System.out.println("You won!!!!!!");
+                        money += bet * PAYOUT_ODDS;
+                    }
                 }
                 else {
+                    money -= bet;
                     System.out.println("You Lose!!!");
+                    //Calculate comps
+                    int c = (int)(Math.random() * 50);
+                    if(c == COMP_FREE_DRINK) {
+                        System.out.println("I feel bad for you, here get yourself a drink!");
+                        money += 5;
+                        compTimesPlayed = 0;
+                    }
+                    else if(c == COMP_FREE_MOVIE) {
+                        System.out.println("You have been playing for a while, go relax with a good movie!");
+                        money += 10;
+                    }
+                    else if(c == COMP_FREE_VACATION && compTimesPlayed > 100) {
+                        System.out.println("You are truly a devoted gambler. The casino will give you a free trip to the Bhamas!!!");
+                        money += 500;
+                    }
                 }
+                timesPlayed++;
+                compTimesPlayed++;
             }
             else {
                 System.out.println("Fine!");
